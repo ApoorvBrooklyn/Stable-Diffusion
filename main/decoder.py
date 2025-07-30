976539f996d6577d415230cf4be6de966a4dc8ac
Apoorv <apoorobot21@gmail.com>
2025-07-30 17:14:40 +0530

    Decoder

--- main/decoder.py
+++ main/decoder.py
@@ -5,6 +5,28 @@ from attention import SelfAttention
 
 
 class VAE_AttentionBlock(nn.Module):
+    def __init__(self, channels: int):
+        super().__init__()
+
+        self.groupnorm = nn.GroupNorm(32, channels)
+        self.attention = SelfAttention(1, channels)
+
+    def forward(self, x : torch.Tensor)-> torch.Tensor:
+        residue = x
+        n, c, h, w = x.shape
+
+        x = x.view(n, c, h*w)
+
+        x = x.transpose(-1,2)
+
+        x = self.attention(x)
+
+        x = x.transpose(-1,2)
+        x = x.view((n, c, h, w))
+
+        x += residue
+
+        return x
     
 
 class VAE_ResidualBlock(nn.Module):
